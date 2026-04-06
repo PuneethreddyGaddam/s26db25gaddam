da04c65b3e222c72f4605ab120b266b8dedc7b49
puneeth reddy gaddam <s561425@nwmissouri.edu>
2026-04-06 18:01:09 -0500

    Add Mongo connection and allow app.js to start server

--- app.js
+++ app.js
@@ -2,6 +2,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var fs = require('fs');
+
+var envPath = path.join(__dirname, 'mongo.env');
+if (fs.existsSync(envPath)) {
+  require('dotenv').config({ path: envPath });
+} else {
+  require('dotenv').config();
+}
+
+var mongoose = require('mongoose');
+if (!process.env.MONGO_URI) {
+  console.warn('MONGO_URI is not set. The app will start without a DB connection.');
+} else {
+  mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
+}
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -25,5 +42,16 @@ app.use('/users', usersRouter);
 app.use('/yurtcampsites', yurtCampsitesRouter);
 app.use('/grid', gridRouter);
 app.use('/selector', pickRouter);
+
+// If app.js is run directly (e.g., `node app.js`), start the server here.
+if (require.main === module) {
+  var http = require('http');
+  var port = process.env.PORT || '3000';
+  app.set('port', port);
+  var server = http.createServer(app);
+  server.listen(port, () => {
+    console.log('Server listening on port ' + port);
+  });
+}
 
 module.exports = app;
